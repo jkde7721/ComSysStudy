52d2f2a08c51df23a4b8c231d4e0459cea18c5e5
greenjee <68232448+greenjee@users.noreply.github.com>
2021-10-31 22:12:58 +0900

    첫 16비트는 칠했지만, 모든 화면을 칠하는 것은 모르겠음
    인덱스의 값을 키우는 방법을 모르겠음

--- 04_project/Fill.asm
+++ 04_project/Fill.asm
@@ -12,3 +12,38 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+@KBD
+D=M
+
+@TOBLACK
+D;JGT // a key is pressed
+@TOWHITE
+D;JEQ // no key is pressed
+
+(TOBLACK)
+@0
+M=-1 // 1111 1111 1111 1111
+
+@FILL
+0;JMP // goto FILL
+
+(TOWHITE)
+@0
+M=0
+
+@FILL
+0;JMP // goto FILL
+
+(FILL)
+@0
+D=M // D= -1 or 0
+
+@SCREEN
+M=D
+
+// 첫 16비트는 칠했는데 그 이후는 잘 모르겠음
+// 인덱스의 값을 키우는 방법을 모르겠음
+
+@LOOP
+0;JMP // goto LOOP
