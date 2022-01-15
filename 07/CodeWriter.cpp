a77a7b848b799b505bcc34c621b9b64ce31c196e
dbsdlqls <yibin99@sookmyung.ac.kr>
2022-01-15 20:53:41 +0900

    segment 처리, 파일 미처리

--- 07/CodeWriter.cpp
+++ 07/CodeWriter.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 
 CodeWriter::CodeWriter() {
-	outputStream.open("C:\\Users\\User\\Desktop\\yibin\\nand2tetris\\projects\\07\\StackArithmetic\\StackTest\\StackTest.asm");
+	outputStream.open("C:\\Users\\User\\Desktop\\yibin\\nand2tetris\\projects\\07\\MemoryAccess\\StaticTest\\StaticTest.asm");
 }
 
 void CodeWriter::setFileName(string fname) {
 	Parser parser(fname);
 	while (parser.hasMoreCommands()) {
 		parser.Parsing();
-		/*test code*/
+		/*test code
 		string s[] = {
 		"C_ARITHMETIC", "C_PUSH", "C_POP", "C_LABEL", "C_GOTO", "C_IF", "C_FUNCTION", "C_RETURN", "C_CALL", "SKIP"
 		};
 		cout << s[parser.getType()] << " " << parser.getArgv1() << " " << parser.getArgv2() << '\n';
-		
+		*/
 		switch (parser.getType()) {
 		case C_ARITHMETIC:
 			writeArithmetic(parser.getArgv1());
@@ -88,14 +88,95 @@ void CodeWriter::writeArithmetic(string command) {
 }
 
 void CodeWriter::writePushPop(type command, string segment, int index) {
-	//constant segment父 贸府
 	switch (command) {
 	case C_PUSH:
-		outputStream << "@" << index << "\nD=A\n";
-		outputStream << "@SP\nA=M\nM=D\n";
-		outputStream << "@SP\nM=M+1\n";
+		if (segment.compare("constant") == 0) {
+			outputStream << "@" << index << "\nD=A\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("local") == 0) {
+			outputStream << "@LCL\nD=M\n@" << index << "\nD=D+A\nA=D\nD=M\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("argument") == 0) {
+			outputStream << "@ARG\nD=M\n@" << index << "\nD=D+A\nA=D\nD=M\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("this") == 0) {
+			outputStream << "@THIS\nD=M\n@" << index << "\nD=D+A\nA=D\nD=M\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("that") == 0) {
+			outputStream << "@THAT\nD=M\n@" << index << "\nD=D+A\nA=D\nD=M\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("pointer") == 0) {
+			outputStream << "@3\nD=A\n@" << index << "\nD=D+A\nA=D\nD=M\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("temp") == 0) {
+			outputStream << "@5\nD=A\n@" << index << "\nD=D+A\nA=D\nD=M\n";
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
+		else if (segment.compare("static") == 0) {
+			outputStream << "@Xxx." << index << "\nD=M\n";//颇老 捞抚 贸府秦具 窃
+			outputStream << "@SP\nA=M\nM=D\n";
+			outputStream << "@SP\nM=M+1\n";
+		}
 		break;
 	case C_POP:
+		outputStream << "@SP\nM=M-1\nA=M\nD=M\n";
+		if (segment.compare("local") == 0) {
+			outputStream << "@LCL\nA=M\n";
+			for(int i=0;i<index;i++)
+				outputStream << "A=A+1\n";
+			outputStream << "M=D\n";
+		}
+		else if (segment.compare("argument") == 0) {
+			outputStream << "@ARG\nA=M\n";
+			for (int i = 0;i < index;i++)
+				outputStream << "A=A+1\n";
+			outputStream << "M=D\n";
+			//outputStream << "@ARG\nD=A\n@" << index << "\nD=D+A\nM=D\n";
+		}
+		else if (segment.compare("this") == 0) {
+			outputStream << "@THIS\nA=M\n";
+			for (int i = 0;i < index;i++)
+				outputStream << "A=A+1\n";
+			outputStream << "M=D\n";
+			//outputStream << "@THIS\nD=A\n@" << index << "\nD=D+A\nM=D\n";
+		}
+		else if (segment.compare("that") == 0) {
+			outputStream << "@THAT\nA=M\n";
+			for (int i = 0;i < index;i++)
+				outputStream << "A=A+1\n";
+			outputStream << "M=D\n";
+			//outputStream << "@THAT\nD=A\n@" << index << "\nD=D+A\nM=D\n";
+		}
+		else if (segment.compare("pointer") == 0) {
+			outputStream << "@3\n";
+			for (int i = 0;i < index;i++)
+				outputStream << "A=A+1\n";
+			outputStream << "M=D\n";
+			//outputStream << "@3\nD=A\n@" << index << "\nD=D+A\nM=D\n";
+		}
+		else if (segment.compare("temp") == 0) {
+			outputStream << "@5\n";
+			for (int i = 0;i < index;i++)
+				outputStream << "A=A+1\n";
+			outputStream << "M=D\n";
+			//outputStream << "@5\nD=A\n@" << index << "\nD=D+A\nM=D\n";
+		}
+		else if (segment.compare("static") == 0) {
+			outputStream << "@Xxx." << index << "\nM=D\n";
+		}
 		break;
 	}
 }
