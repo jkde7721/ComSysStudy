cbb64ea626c606848251faf6422de928c0325495
dlduddk <49145450+dlduddk@users.noreply.github.com>
2022-01-15 21:57:18 +0900

    Delete 07 directory

--- 07/main.py
+++
@@ -1,7 +0,0 @@
-import Parsermodule
-import CodeWritermodule
-
-parser = Parsermodule.Parser("D:\PointerTest.vm")
-codewriter = CodeWritermodule.CodeWriter("D:\PointerTest.asm", "D:\PointerTest.vm", parser.tokens)
-codewriter.setFileName("D:\PointerTest.vm")
-codewriter.codewrite()
